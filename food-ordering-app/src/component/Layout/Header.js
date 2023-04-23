2f7be5347276c3e2d685ede26abf7be9bf074fdc
ShivaMisraa <shivam18557@gmail.com>
2023-04-23 23:35:12 +0530

    Added cart button and header

---
+++ food-ordering-app/src/component/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import mealsImage from "../../assets/Meals.png";
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A photo including smart phone and packet of food!" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
